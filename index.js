99a65f241f350d39d3ad5475b163badd8a2868e6
Audi-ana Brown <audi-anabrown@Audi-anas-MacBook-Pro.local>
2019-04-08 16:20:30 -0500

    Started with the registration and login features

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const models = require('./models')
+const mustacheExpress = require('mustache-express')
+const bcrypt= require('bcrypt')
+const saltRounds = 10;
+const app = express()
+
+
+app.engine('mustache', mustacheExpress())
+
+app.set('views', './views')
+app.set('view engine', 'mustache')
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.get('/',(req, res)=> {
+    res.render('login')
+})
+
+app.get('/register', (req,res)=>{
+    res.render('register')
+})
+
+app.post('/register',(req,res)=>{
+  
+    let username = req.body.username
+    let password = req.body.password 
+
+    
+
+ bcrypt.hash(password, saltRounds, function(error, hash) {
+    models.User.build({
+        username: username,
+        password: hash
+    }).save()
+    }).then(console.log("SUCCESS"))
+     res.redirect('/')
+})
+
+app.listen(3000,function(){
+    console.log("Server is running...")
+  })
