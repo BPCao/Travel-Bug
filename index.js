7c02f2797ae11e40d1f30c1acda479f005c3a9e7
Audi-ana Brown <audi-anabrown@Audi-anas-MacBook-Pro.local>
2019-04-10 09:22:11 -0500

    login and registration is functional

--- index.js
+++ index.js
@@ -6,20 +6,44 @@ const bcrypt= require('bcrypt')
 const saltRounds = 10;
 const app = express()
 
+var session = require('express-session')
+
+
+
+//session setup
+app.use(session({
+    secret:'travelBug',
+    resave:false,
+    saveUninitialized: true
+}))
+app.all('/login/*', authenticate)
 
 app.engine('mustache', mustacheExpress())
-
 app.set('views', './views')
 app.set('view engine', 'mustache')
 
 app.use(bodyParser.urlencoded({ extended: false }))
 
-app.get('/',(req, res)=> {
+function authenticate(req,res,next){
+
+    if(req.session){
+        if(req.session.userId) {
+            // go to the next/original request
+            next()
+          } else {
+            res.redirect('/login')
+          }
+        } else {
+            res.redirect('/login')
+        }
+    }
+
+app.get('/login',(req, res)=> {
     res.render('login')
 })
 
 app.get('/register', (req,res)=>{
-    res.render('register')
+    res.render('register') 
 })
 
 app.post('/register',(req,res)=>{
@@ -30,21 +54,52 @@ app.post('/register',(req,res)=>{
     
 
  bcrypt.hash(password, saltRounds, function(error, hash) {
-    models.User.build({
+    models.User.create({
         username: username,
         password: hash
-    }).save()
-    }).then(console.log("SUCCESS"))
-     res.redirect('/')
+    })
+    .then(console.log("SUCCESS"))
+     res.redirect('/login')
+    })
 })
 
-app.post('/login', (res, req)=>{
-
-    let memberU = req.body.memeberU
+app.post('/login', (req, res)=>{
+    
+    let memberU = req.body.memberU
     let memberP = req.body.memberP
 
-    if()
+    models.User.findOne({
+        where: {
+            username: memberU
+        }
+    })
+    .then(function(user) {
+        if (user === null) {
+            res.render('login', {message: "Sorry invalid username and/or password"})
+        }
 
+        else {
+            bcrypt.compare(memberP, user.password, function(err, result) {
+                if(result) {
+                    if(req.session) {
+                        req.session.userId = user.id 
+                    }
+
+                    res.redirect('/homePage')
+                }
+
+                else {
+                    res.render('login', {message: "Sorry invalid password"})
+                }
+            })
+        }
+    })
+
+
+})
+
+app.get('/login/homePage',(req, res)=>{
+    res.render('homePage')
 })
 
 app.listen(3000,function(){
