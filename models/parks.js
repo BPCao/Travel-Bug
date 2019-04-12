54fe5cbca0bb57524948243a85eccef2fd128067
Bach Cao <bach.cao@Bachs-MacBook-Pro.local>
2019-04-12 13:09:27 -0500

    switched ports

--- models/parks.js
+++ models/parks.js
@@ -3,11 +3,7 @@ module.exports = (sequelize, DataTypes) => {
   const Parks = sequelize.define('Parks', {
     category: DataTypes.STRING,
     userid: DataTypes.STRING,
-<<<<<<< HEAD
     parkid: DataTypes.STRING
-=======
-    parkid: DataTypes.STRING,
->>>>>>> master
   }, {});
   Parks.associate = function(models) {
     // associations can be defined here
